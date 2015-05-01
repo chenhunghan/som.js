846332e109716db92663df6e7f2bef2ed74bcd87
chenhunghan <stk.chen@gmail.com>
2015-05-01 19:03:09 +0300

    setup basic testing env

---
+++ src/ex1.js
@@ -0,0 +1,4 @@
+/**
+ * Created by chh on 2015/5/1.
+ */
+//# sourceMappingURL=ex1.js.map
---
+++ test/ex1Spec.js
@@ -0,0 +1,8 @@
+/**
+ * Created by chh on 2015/5/1.
+ */
+it('should take less than 500ms', function (done) {
+    this.timeout(500);
+    setTimeout(done, 300);
+});
+//# sourceMappingURL=ex1Spec.js.map
