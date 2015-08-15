93d36058fb548c044144eab5d7e6c57c7fe725a7
Hung-Han Chen <chenhungh@gmail.com>
2015-08-15 19:34:43 +0300

    modify demo a little bit

--- example/64nodeSOM.js
+++ example/64nodeSOM.js
@@ -4,8 +4,13 @@ import cwise from "cwise"
 import show from 'ndarray-show'
 import Som from "../dist/som.js"
 
+<<<<<<< HEAD
 var modelNumber = 16,
     dimension = 2, //2 -> [x,y], 3 -> [x,y,z],
+=======
+var modelNumber = 64,
+    dimension = 4, //2 -> [x,y], 3 -> [x,y,z],
+>>>>>>> modify demo a little bit
     M = ndarray(new Float32Array(modelNumber*dimension), [dimension, modelNumber]),
     sqrootM = Math.floor(Math.sqrt(modelNumber)),
     inputVector = ndarray(new Float32Array(dimension*modelNumber), [dimension, modelNumber]),
@@ -18,10 +23,21 @@ var som = new Som(M)
 
 console.log('---------- original Map')
 console.log('')
+<<<<<<< HEAD
 console.log(show(ndarray(new Float32Array(M.data), [dimension, sqrootM, sqrootM])))
 console.log('---------- input')
 console.log('')
 console.log(show(ndarray(new Float32Array(inputVector.data), [dimension, sqrootM, sqrootM])))
+=======
+console.log(show(ndarray(new Float32Array(M.data), [sqrootM, sqrootM, dimension])))
+
+//console.log('---------- input')
+//console.log('')
+//console.log(show(ndarray(new Float32Array(inputVector.data), [sqrootM, sqrootM, dimension])))
+
+M = som.learn(M, inputVector, trainingTimes)
+
+>>>>>>> modify demo a little bit
 console.log('---------- trained Map')
 console.log('')
 var trainedMap = som.learn(inputVector, trainingTimes)
