cac9db5a2c6d8318a9c22365316f864ea8e98296
Hung-Han Chen <chenhungh@gmail.com>
2015-08-19 08:41:20 +0300

    working version of example in es6

--- example/64nodeSOM.js
+++ example/64nodeSOM.js
@@ -1,12 +1,12 @@
-var ndarray = require("ndarray")
-var ops = require("ndarray-ops")
-var show =  require('ndarray-show')
-var Som = require("../dist/som.js")
+import ndarray from "ndarray"
+import ops from "ndarray-ops"
+import cwise from "cwise"
+import show from 'ndarray-show'
+import Som from "../dist/som.js"
 
 var modelNumber = 16,
-    dimension = 1, //2 -> [x,y], 3 -> [x,y,z],
+    dimension = 2, //2 -> [x,y], 3 -> [x,y,z],
     M = ndarray(new Float32Array(modelNumber*dimension), [dimension, modelNumber]),
-
     sqrootM = Math.floor(Math.sqrt(modelNumber)),
     inputVector = ndarray(new Float32Array(dimension*modelNumber), [dimension, modelNumber]),
     trainingTimes = 20;
@@ -18,11 +18,11 @@ var som = new Som(M)
 
 console.log('---------- original Map')
 console.log('')
-console.log(show(ndarray(new Float32Array(M.data), [sqrootM, sqrootM, dimension])))
+console.log(show(ndarray(new Float32Array(M.data), [dimension, sqrootM, sqrootM])))
 console.log('---------- input')
 console.log('')
-console.log(show(ndarray(new Float32Array(inputVector.data), [sqrootM, sqrootM, dimension])))
+console.log(show(ndarray(new Float32Array(inputVector.data), [dimension, sqrootM, sqrootM])))
 console.log('---------- trained Map')
 console.log('')
-//var trainedMap = som.learn(inputVector, trainingTimes)
-//console.log(show(ndarray(new Float32Array(trainedMap.data), [sqrootM, sqrootM, dimension])))
+var trainedMap = som.learn(inputVector, trainingTimes)
+console.log(show(ndarray(new Float32Array(trainedMap.data), [dimension, sqrootM, sqrootM])))
